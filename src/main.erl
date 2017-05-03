e5fbc3cf42fb42a5dad91955757434d4ad570b9d
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-05-03 20:05:21 +0200

    Use explicit arguments.

--- src/main.erl
+++ src/main.erl
@@ -5,6 +5,7 @@
 % -define(SITE, "http://web.student.chalmers.se/~hanghj/map-reduce/a").
 -define(SITE, "http://127.0.0.1:8080").
 -define(DEPTH, 4).
+-define(DATA_FILE, "./web.dat").
 
 dbg(Frmt) -> dbg(Frmt, []).
 dbg(Frmt, X) -> io:format(Frmt, X).
@@ -13,8 +14,8 @@ main() ->
   inets:start(),
   Res = crawl:crawl(?SITE, ?DEPTH),
   dbg("~p~n", [Res]),
-  {ok, F} = dets:open_file("./web.dat", []),
+  {ok, F} = dets:open_file(?DATA_FILE, []),
   dets:insert(F, Res),
-  Lkp = dets:lookup(F, ?SITE),
+  % Lkp = dets:lookup(F, ?SITE),
   inets:stop(),
-  Lkp.
+  page_rank:page_rank(?DATA_FILE).
--- src/page_rank.erl
+++ src/page_rank.erl
@@ -15,15 +15,14 @@ map(Url,ok) ->
 reduce(Url,Ns) ->
     [{Url,lists:sum(Ns)}].
 
-page_rank() ->
-    {ok,web} = dets:open_file(web,[{file,"web.dat"}]),
+page_rank(File) ->
+    {ok,web} = dets:open_file(web,[{file,File}]),
     Urls = dets:foldl(fun({K,_},Keys)->[K|Keys] end,[],web),
     map_reduce:map_reduce_seq(fun map/2, fun reduce/2,
                   [{Url,ok} || Url <- Urls]).
 
-page_rank_par() ->
-    dets:open_file(web,[{file,"web.dat"}]),
+page_rank_par(File) ->
+    dets:open_file(web,[{file,File}]),
     Urls = dets:foldl(fun({K,_},Keys)->[K|Keys] end,[],web),
     map_reduce:map_reduce_par(fun map/2, 32, fun reduce/2, 32,
                   [{Url,ok} || Url <- Urls]).
-
