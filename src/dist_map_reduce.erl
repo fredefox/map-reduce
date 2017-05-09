bd0e11402f5a88ac667360af04850c317bac7d5f
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2017-05-09 09:59:45 +0200

    Fix syntax errors and the like

--- src/dist_map_reduce.erl
+++ src/dist_map_reduce.erl
@@ -21,28 +21,28 @@ spawn_reducers(Reduce,Call,Master,[],Nodes,NR, NM) ->
   spawn_reducers(Reduce, Call, Master, Nodes, Nodes, NR, NM);
 spawn_reducers(_,_,_,_,_, 0, _) -> [];
 spawn_reducers(Reduce,Call,Master,[N|Ns], Nodes, NR, NM) ->
-  [rpc:call(N, erlang, spawn, [fun () -> reducer(Reduce,Call,Master,NM) end]) | 
+  [rpc:call(N, erlang, spawn, [fun () -> reducer(Reduce,Call,Master,NM) end]) ||
    spawn_reducers(Reduce,Call,Master,Ns,Nodes,NR-1, NM)].
 
 spawn_mappers(Map, Chunks, Call, Reducers, IO_Info) ->
   Zipd = zip_round_robin(Chunks, IO_Info#io_info.nodes),
   Red = list_to_tuple(Reducers),
-  [rpc:call(N, erlang, spawn, [fun () -> mapper(Map, C, Red) end] || {C, N} <- Zipd].
+  [rpc:call(N, erlang, spawn, [fun () -> mapper(Map, C, Red, Call) end]) || {C, N} <- Zipd].
 %%DS[k] += v
 
 % Reducers :: Array Pid
-mapper(F, Xs, Reducers) ->
+mapper(F, Xs, Reducers, Call) ->
   N = length(Reducers),
   [ R ! {Call, more_stuff,  X}
     || X = {K, _} <- lists:map(F, Xs)
-    ,  RIndx = phash2(K, N)
-    , R <- element(RIndx, Reducers)
+    ,  RIndx = erlang:phash2(K, N)
+    ,  R <- element(RIndx+1, Reducers)
   ],
   [ R ! {Call, done} || R <- tuple_to_list(Reducers)].
 
 zip_round_robin(As, Bs) -> zip_round_robin(As, Bs, Bs).
-zip_round_robin([], _, _) -> [].
-zip_round_robin(As, [], Bs2) -> zip_round_robin(As, Bs2, Bs2).
+zip_round_robin([], _, _) -> [];
+zip_round_robin(As, [], Bs2) -> zip_round_robin(As, Bs2, Bs2);
 zip_round_robin([A|As], [B|Bs], Bs2) -> [{A, B} | zip_round_robin(As, Bs, Bs2)].
 
 receive_reducer_output(Call, NR) ->
@@ -68,4 +68,4 @@ group([]) -> [];
 group([{K, V} | Xs]) -> group(K, [V], Xs).
 group(K, Vs, []) -> [{K,Vs}];
 group(K, Vs, [{K,V}|Xs])-> group(K, [V|Vs], Xs);
-group(K, Vs, [{K2,V}|Xs]) -> [{K,Vs} | group(K2,[V],Xs)]
+group(K, Vs, [{K2,V}|Xs]) -> [{K,Vs} | group(K2,[V],Xs)].
